9e4fb0b72d06d3f90e9cd7e82daa1008aa01b71d
lukeouyang <luke_ou@hotmail.com>
2021-02-01 00:45:27 -0800

    feat: add config file

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": "postgres",
+    "aws_region": "us-west-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "petalbum-lukeouyang-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "petalbum_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
